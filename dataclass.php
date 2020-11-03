0a55d50cc5b62e6e61278950802a3bc38211b020
Leonardo Wrobel <leonardo.wrobel@gmail.com>
2020-11-03 16:44:53 -0300

    add comments

--- dataclass.php
+++ dataclass.php
@@ -1,5 +1,6 @@
 <?php
 
+// Google config
 require __DIR__ . '/vendor/autoload.php';
 
 $client = new \Google_Client();
@@ -11,50 +12,55 @@ $client->setAccessType('offline');
 $service = new Google_Service_Sheets($client);
 $spreadsheetId = "1ls_2SPIzClGAWovZ7-Xau0IO6OQT_xqNr8QxuwbTmUQ";
 
+// Especify where(range) to read on spreadsheet
 $rangeGrades = "Semester_1!A4:F27";
+// Especify where to find total classes on spreadsheet
 $rangeTotalClasses = "Semester_1!C2";
+// Get data and set local variables
 $response = $service->spreadsheets_values->get($spreadsheetId, $rangeGrades);
 $valuesGrades = $response->getValues();
 $response = $service->spreadsheets_values->get($spreadsheetId, $rangeTotalClasses);
 $totalClasses = $response->getValues()[0][0];
 $maxClassAbsences = round(25 * $totalClasses / 100);
 
+// Especify where(range) to edit on spreadsheet
 $rangeResults = "Semester_1!G4:H27";
 $valuesResults = array();
 
 if (empty($valuesGrades)) {
+    // 
     print "No data.\r\n";
 } else {
-    //echo "\r\n============================================\r\n\r\n";
+    // Business logic
     foreach ($valuesGrades as $row) {
         $gradesAvg = round(($row[3] + $row[4] + $row[5]) / 3);
         $status = "";
-        $msp = "";
-        //echo $row[0] . " - " . $row[1] . " - AVG: " . $gradesAvg . " - ";
-        if ($row[2] > $maxClassAbsences) {
-            //echo " Failed by absences.\r\n";
+        $msp = "";        
+        if ($row[2] > $maxClassAbsences) {            
             $status = "Reprovado";
         } else {
-            if ($gradesAvg >= 70) {
-                //echo " Pass.\r\n";
+            if ($gradesAvg >= 70) {                
                 $status = "Aprovado";
             } else {
                 $msp = 100 - $gradesAvg;
-                $status = "Final";
-                //echo " Final Exam. [Minimum Score to Pass:" . $msp . "]\r\n";
+                $status = "Final";                
             }
         }
         $valuesResults[] = [$status, $msp];
     }
-    //foreach($valuesResults as $row){   
-    //    echo " - " . $row[0] . " - " . $row[1] . "\r\n";
-    //} 
+    // Test generated data 
+    /* foreach($valuesResults as $row){   
+        echo " - " . $row[0] . " - " . $row[1] . "\r\n";
+    } */ 
+    
+    // Preparing to edit remote spreadsheet
     $body = new Google_Service_Sheets_ValueRange([
         'values' => $valuesResults
     ]);
     $params = [
         'valueInputOption' => 'RAW'
     ];
+    // Execute the changes on remote spreadsheet
     $result = $service->spreadsheets_values->update(
         $spreadsheetId,
         $rangeResults,
